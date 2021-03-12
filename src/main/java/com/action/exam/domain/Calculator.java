17841805964f76df7340f80d24001e499c1a8e48
w-beom <w_bum@naver.com>
2021-03-12 17:43:35 +0900

    뺴기 테스트

--- src/main/java/com/action/exam/domain/Calculator.java
+++ src/main/java/com/action/exam/domain/Calculator.java
@@ -6,4 +6,8 @@ public class Calculator {
     public int plus(int a, int b) {
         return a + b;
     }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
 }
--- src/test/java/com/action/exam/domain/CalculatorTest.java
+++ src/test/java/com/action/exam/domain/CalculatorTest.java
@@ -1,16 +1,23 @@
 package com.action.exam.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CalculatorTest {
-
     @Test
     @DisplayName("더하기 테스트")
     void plusTest() {
         Calculator calculator = new Calculator();
         assertThat(calculator.plus(1, 2)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("빼기 테스트")
+    void minusTest() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.minus(1, 2)).isEqualTo(-1);
     }
 }
